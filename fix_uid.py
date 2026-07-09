671216af5a81aedf16bda54935682c3238827a32
saintwolf95 <sebas.avello1@gmail.com>
2026-07-09 10:02:45 +0200

    Security: Remove leaked env file and hardcoded fix script

--- fix_uid.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy import text
-
-engine = create_engine('sqlite:///supplychain.db')
-with engine.connect() as conn:
-    conn.execute(text("UPDATE usuarios SET supabase_uid='34cf496c-0ca3-41db-bb63-b39078963a24' WHERE email='admin@demo.com'"))
-    conn.commit()
